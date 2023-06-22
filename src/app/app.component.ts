d96292f9f0bacd9a41871e9f38bf863395895276
ianacodev <ianaco.dev@gmail.com>
2023-06-21 22:51:02 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 // services
 import { AppService } from './app.service';
 
@@ -8,10 +9,24 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private appService: AppService) {}
+  readonly VAPID_PUBLIC_KEY =
+    'BId1LdU7Kcq6UbvVDQmia-hKIChYJguBxb19eFToDCDh5bBIwjp-QnKkLOoK3L1j6w3WbVxF1PhMwyo-Cd-DMWU';
+  constructor(private appService: AppService, private swPush: SwPush) {}
 
   ngOnInit(): void {
     this.appService.logUpdate();
     this.appService.promptUpdate();
+  }
+
+  subscribeToNotifications() {
+    console.log('subscribe');
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((sub) => console.log('record', sub))
+      .catch((err) =>
+        console.error('Could not subscribe to notifications', err)
+      );
   }
 }
