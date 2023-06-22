140d811f554177f01a14f96a7a3a93539eb79cdd
ianacodev <ianaco.dev@gmail.com>
2023-06-21 21:34:00 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// services
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample-pwa-app';
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.appService.logUpdate();
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,93 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ApplicationRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { concat, interval } from 'rxjs';
+import { map, first, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private appRef: ApplicationRef,
+    private updates: SwUpdate,
+    private http: HttpClient
+  ) {}
 
   getDogBreeds() {
     const url = 'https://dog.ceo/api/breeds/list/all';
     return this.http.get<any>(url).pipe(map((res) => Object.keys(res.message)));
+  }
+
+  logUpdate() {
+    this.updates.versionUpdates.subscribe((evt) => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(
+            `New app version ready for use: ${evt.latestVersion.hash}`
+          );
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(
+            `Failed to install app version '${evt.version.hash}': ${evt.error}`
+          );
+          break;
+      }
+    });
+  }
+
+  checkForUpdates() {
+    const appIsStable$ = this.appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    everySixHoursOnceAppIsStable$.subscribe(async () => {
+      try {
+        const updateFound = await this.updates.checkForUpdate();
+        console.log(
+          updateFound
+            ? 'A new version is available.'
+            : 'Already on the latest version.'
+        );
+      } catch (err) {
+        console.error('Failed to check for updates:', err);
+      }
+    });
+  }
+
+  promptUpdate() {
+    this.updates.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+      )
+      .subscribe((evt) => {
+        if (this.promptUser(evt)) {
+          // Reload the page to update to the latest version.
+          document.location.reload();
+        }
+      });
+  }
+
+  promptUser(evt: any) {
+    return window.confirm('New Version Available Reload to Update?');
+  }
+
+  handleUnrecoverableEvent() {
+    this.updates.unrecoverable.subscribe((event) => {
+      this.notifyUser(
+        'An error occurred that we cannot recover from:\n' +
+          event.reason +
+          '\n\nPlease reload the page.'
+      );
+    });
+  }
+
+  notifyUser(message: string) {
+    window.alert(message);
   }
 }
